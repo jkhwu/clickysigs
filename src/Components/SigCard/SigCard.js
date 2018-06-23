508f4887824a6a32f41c2ace423cd34f1cf7062e
Janet Hwu <Janet@Janets-Laptop.local>
2018-06-23 13:05:13 -0700

    components in progress

---
+++ src/Components/SigCard/SigCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SigCard.css";
+
+const SigCard = props => (
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+export default SigCard;
